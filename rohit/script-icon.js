d13ae6d7e40a86f115be9e30cfe548e93f013d11
rohbdn07 <57314666+rohbdn07@users.noreply.github.com>
2020-02-24 13:18:25 +0200

    added social media icon and footer bar on scroll

--- rohit/script-icon.js
+++ rohit/script-icon.js
@@ -1,12 +1,33 @@
-$(document).ready(function () {
-    $('#share').click(function () {
-        $('#share').toggleClass('rotate');
-        $('.facebook').toggleClass('rotate-fb');
-        $('.twitter').toggleClass('rotate-tw');
-        $('.linkedin').toggleClass('rotate-li');
-        $('.codepen').toggleClass('rotate-cp');
+// $(document).ready(function () {
+//     $('#share').click(function () {
+//         $('#share').toggleClass('rotate');
+//         $('.facebook').toggleClass('rotate-fb');
+//         $('.twitter').toggleClass('rotate-tw');
+//         $('.linkedin').toggleClass('rotate-li');
+//         $('.codepen').toggleClass('rotate-cp');
 
+//     });
 
-    });
+// });
 
-});
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "-200px";
+//   } else {
+//     document.getElementById("navbar").style.top = "0px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// };
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (currentScrollPos < prevScrollpos) {
+    document.getElementById("flex").style.top = "-500px";
+  } else {
+    document.getElementById("flex").style.top = "950px";
+  }
+  prevScrollpos = currentScrollPos;
+};
