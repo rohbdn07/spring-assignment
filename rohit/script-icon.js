786d8854b026965f79db7f907d3a84eb873d2808
rohbdn07 <57314666+rohbdn07@users.noreply.github.com>
2020-02-24 14:37:54 +0200

    some media query is added

--- rohit/script-icon.js
+++ rohit/script-icon.js
@@ -21,6 +21,7 @@
 //   prevScrollpos = currentScrollPos;
 // };
 
+// ----------------------used for footer-bar------------------
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
   var currentScrollPos = window.pageYOffset;
@@ -31,3 +32,21 @@ window.onscroll = function() {
   }
   prevScrollpos = currentScrollPos;
 };
+
+/*Scroll to top when arrow up clicked BEGIN*/
+$(window).scroll(function() {
+  var height = $(window).scrollTop();
+  if (height > 100) {
+    $("#back2Top").fadeIn();
+  } else {
+    $("#back2Top").fadeOut();
+  }
+});
+$(document).ready(function() {
+  $("#back2Top").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
+});
+/*Scroll to top when arrow up clicked END*/
